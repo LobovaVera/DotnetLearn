ae2701bb1f0839a2a1925c05258dcc97e0b12346
Vera Lobova <pchilli@yandex.ru>
2022-09-27 15:00:48 +0300

    Condition if-else

--- EX005_if_else/Program.cs
+++ EX005_if_else/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите имя пользователя!");
-username = Console.ReadLine();
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это Маша!");
 
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
