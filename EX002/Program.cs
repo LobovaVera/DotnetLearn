372d8bda78f5c40af6c5cefdbdab3e99ad797ff0
Vera Lobova <pchilli@yandex.ru>
2022-09-27 12:50:34 +0300

    error cs8600 treated using !

--- EX002/Program.cs
+++ EX002/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Enter your name ");
-string nickname = Console.ReadLine ();
+string nickname = Console.ReadLine()!;
 Console.Write("Hello, ");
 Console.WriteLine(nickname);
 
